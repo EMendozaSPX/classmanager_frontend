b5c2be5ecbb67529f5edf04941f676813b6642a3
Euan Mendoza <emendozaspx@gmail.com>
2019-04-25 14:37:40 +1000

    fixed mutation

--- src/mutations.ts
+++ src/mutations.ts
@@ -2,9 +2,7 @@ import gql from 'graphql-tag';
 
 const LOGIN_MUTATION = gql`
     mutation Login($userType: userType!, $username: String!, $password: String!) {
-        login(userType: $userType, username: $username, password: $password) {
-            token
-        }
+        login(userType: $userType, username: $username, password: $password)
     }
 `;
 

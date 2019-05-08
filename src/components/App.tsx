888412eb5e82c70ab05f20c1d4809c80e9910022
emendoza <emendozaspx@gmail.com>
2019-05-08 18:56:21 +1000

    bug fixes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -49,7 +49,7 @@ const App = () => {
                     <Switch>
                         <Route exact path="/" component={Landing} />
                         <Route exact path="/admin" component={AdminDashboard} />
-                        <Route path="/:userType(admin|teacher|student)/login" component={Login} />
+                        <Route path="/:usertype(admin|teacher|student)/login" component={Login} />
                         <Route component={NoMatch} />
                     </Switch>
                 </MuiThemeProvider>

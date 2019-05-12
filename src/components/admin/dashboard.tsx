5acb46981b7d03f8aa1f70eea90be8d1cad0ca28
Euan Mendoza <emendozaspx@gmail.com>
2019-05-12 20:40:26 +1000

    added styles to admin dash

--- src/components/admin/dashboard.tsx
+++ src/components/admin/dashboard.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useQuery } from 'react-apollo-hooks';
 import { Redirect } from 'react-router-dom';
-import { createStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, withStyles, WithStyles, Theme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles'
 
 import { GET_AUTHORIZATION } from '../../queries';
 
 const styles = (theme: Theme) => createStyles({
-
+    root: {
+        display: 'flex'
+    }
 });
 
-const AdminDashboard = (props: any) => {
+interface AdminDashProps extends WithStyles<typeof styles>{}
+
+const AdminDashboard = (props: AdminDashProps) => {
+    const { classes } = props;
     const { data, error, loading } = useQuery(GET_AUTHORIZATION, {
         variables: {
             role: 'admin'
@@ -28,7 +34,10 @@ const AdminDashboard = (props: any) => {
     }
 
     return (
-        <div>
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="static">
+            </AppBar>
             <Typography component="h2" variant="h1">Hello Admin Dashboard</Typography>
         </div>
     )

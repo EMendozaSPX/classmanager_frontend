546e9394b60ddf0e62859718dbf806f25056f5b4
Euan Mendoza <emendozaspx@gmail.com>
2019-06-06 21:09:13 +1000

    test props

--- src/components/teacher/class.tsx
+++ src/components/teacher/class.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBack from '@material-ui/icons/ArrowBack';
+import Typography from '@material-ui/core/Typography';
 
 import LinkAdapter from '../link-adapter';
 
-const ClassView = () => {
+interface ClassViewProps extends RouteComponentProps{}
+
+const ClassView = (props: ClassViewProps) => {
+    console.log(props);
     return (
         <div>
             <CssBaseline />
@@ -15,7 +20,9 @@ const ClassView = () => {
                 <IconButton aria-label="back" component={LinkAdapter} to="/dashboard">
                     <ArrowBack />
                 </IconButton>
-
+                <Typography variant="h5" align="center" noWrap>
+                    Test
+                </Typography>
             </Toolbar>
         </div>
     )
